22c330b227d788a9c3761ad6813a5bab5bd83586
관민 이 <rhksals7864@naver.com>
2020-09-24 16:52:38 +0900

    4주차 과제 중간 업로드

---
+++ Client/work4.cpp
@@ -0,0 +1,124 @@
+#pragma comment(lib, "ws2_32")
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+#define BUFSIZE 512
+
+void err_quit(char* msg) {
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, (LPCTSTR)msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(-1);
+}
+
+void err_display(const char* msg) {
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s", msg, (LPCTSTR)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+BOOL GetIPAddr(char* name, IN_ADDR* addr)
+{
+	HOSTENT* ptr = gethostbyname(name);
+	if (ptr == NULL) {
+		err_display("gethostbyname()");
+		return FALSE;
+	}
+
+	memcpy(addr, ptr->h_addr, ptr->h_length);
+	return TRUE;
+}
+
+BOOL GetDomainName(IN_ADDR addr, char* name) {
+	HOSTENT* ptr = gethostbyaddr((char*)&addr,	sizeof(addr), AF_INET);
+	
+	if (ptr == NULL) {
+		err_display("gethostbyaddr()");
+		return FALSE;
+	}
+
+	strcpy(name, ptr->h_name);
+	return FALSE;
+}
+
+int main(int argc, char* argv[]) {
+	int retval;
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		return -1;
+	}
+
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET)
+		err_quit((char*)"socket()");
+
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(9000);
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	retval = bind(listen_sock, (SOCKADDR*)&serveraddr,
+		sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) {
+		err_quit((char*)"bind()");
+	}
+
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) {
+		err_quit((char*)"listen()");
+	}
+
+	SOCKET client_sock;
+	SOCKADDR_IN clientaddr;
+	int addrlen;
+	char buf[BUFSIZE + 1];
+	while (1) {
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
+		if (client_sock == INVALID_SOCKET) {
+			err_display((char*)"accept()");
+			continue;
+		}
+		printf("\n[TCP 서버] 클라이언트 접속: IP주소 = &s, 포트 번호 = %d\n",
+			inet_ntoa(clientaddr.sin_addr),
+			ntohs(clientaddr.sin_port));
+		while (1) {
+			retval = recv(client_sock, buf, BUFSIZE, 0);
+			if (retval == SOCKET_ERROR) {
+				err_display((char*)"recv()");
+				break;
+			}
+			else if (retval == 0) {
+				break;
+			}
+			else {
+				buf[retval] = '\0';
+				printf("%s", buf);
+			}
+		}
+
+	}
+
+	closesocket(client_sock);
+	printf("\n[TCP 서버] 클라이언트 종료: IP주소 = &s, 포트 번호 = %d\n",
+		inet_ntoa(clientaddr.sin_addr),
+		ntohs(clientaddr.sin_port));
+	closesocket(listen_sock);
+
+	WSACleanup();
+	return 0;
+}
